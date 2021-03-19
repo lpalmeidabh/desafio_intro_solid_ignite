27d9c50ce69fdf6b9df871d5f4af76e9a2a0303e
Lucas Almeida <lpalmeidabh@gmail.com>
2021-03-19 01:22:20 -0300

    Testes completos

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,13 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    let user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("User not Found");
+    }
+
+    user = this.usersRepository.turnAdmin(user);
+    return user;
   }
 }
 
